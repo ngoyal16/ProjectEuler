c4c13d7c4c49030307b4ecdbf9fe6f7a38ca5358
Nitin Goyal <nitingoyal.dev@gmail.com>
2017-05-01 19:57:34 +0530

    Delete solution.py

--- Project Euler #48- Self powers/solution.py
+++
@@ -1,6 +0,0 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-L = input()    
-d = 10   # last d digits of series
-s = sum(pow(n, n, 10**d) for n in range(1, L+1))
- 
-print (s%10**d)
